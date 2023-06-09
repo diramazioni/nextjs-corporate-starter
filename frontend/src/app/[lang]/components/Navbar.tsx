03a41dbddea3235bd0f2c8276960c39136eefc69
Eli Spizzichino <scrivi@gmail.com>
2023-06-09 23:48:31 +0200

    Fixing logo look

--- frontend/src/app/[lang]/components/Navbar.tsx
+++ frontend/src/app/[lang]/components/Navbar.tsx
@@ -53,7 +53,7 @@ export default function Navbar({
   };
   return (
     <div className="p-4 bg-col dark:text-gray-100">
-      <div className="container flex justify-between h-16 mx-auto px-0 sm:px-6">
+      <div className="container flex justify-between h-32 mx-auto px-0 sm:px-6">
         <Logo src={logoUrl}>
           {logoText && <h2 className="text-2xl font-bold">{logoText}</h2>}
         </Logo>
