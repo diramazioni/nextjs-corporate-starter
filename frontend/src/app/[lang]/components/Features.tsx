f0da42f8fa1a311f76531bfbad39f8d1707348b7
Eli Spizzichino <scrivi@gmail.com>
2023-06-12 10:16:02 +0200

    add HighlightedText to the title of the Features

--- frontend/src/app/[lang]/components/Features.tsx
+++ frontend/src/app/[lang]/components/Features.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import HighlightedText from "./HighlightedText";
 
 interface FeaturesProps {
   data: {
@@ -56,7 +57,13 @@ export default function Features({ data }: FeaturesProps) {
   return (
     <section className="bg-col dark:text-gray-100 m:py-12 lg:py-24">
       <div className="container mx-auto py-4 space-y-2 text-center">
-        <h2 className="text-5xl font-bold">{data.heading}</h2>
+      <HighlightedText
+            text={data.heading}
+            tag="h2"
+            className="text-5xl font-bold"
+            color="dark:text-teal-400"
+          />
+        
         <p className="dark:text-gray-400">{data.description}</p>
       </div>
       <div className="container mx-auto my-6 grid justify-center gap-4 sm:grid-cols-2 lg:grid-cols-3">
