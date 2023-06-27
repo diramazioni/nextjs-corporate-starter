9c9ec822901eff35521ccedde1ec91ffcc08bb89
Eli Spizzichino <scrivi@gmail.com>
2023-06-27 19:54:44 +0200

    allows to find uploaded SVG in production 5

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,16 +12,16 @@ const nextConfig  =  {
         port: '',
         pathname: '/uploads/**',
       },
-      {
+/*       {
         protocol: 'http',
         hostname: 'localhost',
         port: '1337',
         pathname: '/uploads/**',
-      }
+      } */
     ],
-    dangerouslyAllowSVG: true,
+/*     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;", */
   },
 }
 
